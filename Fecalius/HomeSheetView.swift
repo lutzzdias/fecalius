ca3f0a6b689d6a08e3411d22219f9c3eb7af345e
lutzzdias <thiagolutzdias@hotmail.com>
2024-02-16 09:39:17 +0000

    feat: remove list background, improve section styling and transform + into a button

--- Fecalius/HomeSheetView.swift
+++ Fecalius/HomeSheetView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct HomeSheetView: View {
+    @State var showingAddPoopSheet = false
+    
     var body: some View {
         VStack(spacing: 0) {
             HStack {
@@ -16,57 +18,90 @@ struct HomeSheetView: View {
                     .fontWeight(.bold)
                 
                 Spacer()
-                Image(systemName: "plus")
-                    .resizable()
-                    .padding(4)
-                    .frame(width: 30, height: 30)
+                Button {
+                    showingAddPoopSheet.toggle()
+                } label: {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .padding(4)
+                        .frame(width: 30, height: 30)
+                }
                 
             }
             .padding()
             
-                List {
-                    Section("Recents") {
-                        ForEach(Poop.mock.prefix(3)) { poop in
-                            HStack {
-                                Image(systemName: "mappin.circle.fill")
-                                    .resizable()
-                                    .frame(width: 32, height: 32)
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundStyle(.red.gradient)
-                                
-                                VStack(alignment: .leading) {
+            List {
+                Section {
+                    ForEach(Poop.mock.prefix(3)) { poop in
+                        HStack {
+                            Image(systemName: "mappin.circle.fill")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundStyle(.red.gradient)
+                            
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Text("Home")
+                                        .fontWeight(.semibold)
+                                    Spacer()
                                     HStack {
-                                        Text("Home")
-                                            .fontWeight(.semibold)
-                                        Spacer()
-                                        HStack {
-                                            Image(systemName: "star.fill")
-                                                .resizable()
-                                                .frame(width: 16, height: 16)
-                                            Image(systemName: "star.fill")
-                                                .resizable()
-                                                .frame(width: 16, height: 16)
-                                            Image(systemName: "star.fill")
-                                                .resizable()
-                                                .frame(width: 16, height: 16)
-                                            Image(systemName: "star.fill")
-                                                .resizable()
-                                                .frame(width: 16, height: 16)
-                                            Image(systemName: "star")
-                                                .resizable()
-                                                .frame(width: 16, height: 16)
-                                        }
+                                        Image(systemName: "star.fill")
+                                            .resizable()
+                                            .frame(width: 16, height: 16)
+                                        Image(systemName: "star.fill")
+                                            .resizable()
+                                            .frame(width: 16, height: 16)
+                                        Image(systemName: "star.fill")
+                                            .resizable()
+                                            .frame(width: 16, height: 16)
+                                        Image(systemName: "star.fill")
+                                            .resizable()
+                                            .frame(width: 16, height: 16)
+                                        Image(systemName: "star")
+                                            .resizable()
+                                            .frame(width: 16, height: 16)
                                     }
-                                    
-                                    Text(Date.now.formatted(date: .abbreviated, time: .shortened))
-                                        .font(.caption)
-                                        .foregroundStyle(.secondary)
                                 }
+                                
+                                Text(Date.now.formatted(date: .abbreviated, time: .shortened))
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
                             }
                         }
                     }
+                } header: {
+                    HStack {
+                        Text("Recents")
+                            .font(.body)
+                            .fontWeight(.semibold)
+                            .textCase(nil)
+                            .presentationBackground(.ultraThickMaterial)
+                        
+                        Spacer()
+                        
+                        Button {
+                            print("more")
+                        } label: {
+                            Text("More")
+                                .font(.body)
+                                .textCase(nil)
+                                .foregroundStyle(.link)
+                        }
+                    }
+                    .listRowInsets(EdgeInsets())
+                    .padding(.bottom, 8)
                 }
+            }
+            .listStyle(.insetGrouped)
+            .scrollContentBackground(.hidden)
+            
             Spacer()
+        }
+        .sheet(isPresented: $showingAddPoopSheet) {
+            NavigationStack {
+                AddPoopSheetView()
+            }
         }
     }
 }
